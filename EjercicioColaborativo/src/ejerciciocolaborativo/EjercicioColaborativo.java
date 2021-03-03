1df8fa9494b09fba2aa794cb68970911b7cc2d61
mcm-source <martinmcm@hotmail.es>
2021-03-03 20:16:05 +0100

    Estructura proyecto

---
+++ EjercicioColaborativo/src/ejerciciocolaborativo/EjercicioColaborativo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ejerciciocolaborativo;
+
+/**
+ *
+ * @author marti
+ */
+public class EjercicioColaborativo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

ba23fd59583fc746238819e44fb79aabe694bf52
xoelf <xoelf@LAPTOP-6HU9GKQA>
2021-03-03 20:49:32 +0100

    commit Xoel

--- EjercicioColaborativo/src/ejerciciocolaborativo/EjercicioColaborativo.java
+++ EjercicioColaborativo/src/ejerciciocolaborativo/EjercicioColaborativo.java
@@ -19,7 +19,7 @@ public class EjercicioColaborativo {
         System.out.println("Proba bea");
         System.out.println("Proba Xoel");
         System.out.println("Proba Bea");
-        
+        System.out.println("Proba Xoel 2");
     }
     
 }

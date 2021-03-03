4afa626149db287f142f717605c66aaf4bc474c9
xoelf <xoelf@LAPTOP-6HU9GKQA>
2021-03-03 20:31:20 +0100

    this is the new shit

--- EjercicioColaborativo/src/ejerciciocolaborativo/EjercicioColaborativo.java
+++ EjercicioColaborativo/src/ejerciciocolaborativo/EjercicioColaborativo.java
@@ -17,6 +17,7 @@ public class EjercicioColaborativo {
      */
     public static void main(String[] args) {
         System.out.println("Proba bea");
+        System.out.println("Proba Xoel");
     }
     
 }

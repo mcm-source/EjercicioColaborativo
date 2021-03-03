3ffe9006a2b825384108b89a1ea3fd4c55ac9e1c
mcm-source <martinmcm@hotmail.es>
2021-03-03 21:30:30 +0100

    Creacion paquete clases

--- EjercicioColaborativo/src/ejerciciocolaborativo/EjercicioColaborativo.java
+++ EjercicioColaborativo/src/ejerciciocolaborativo/EjercicioColaborativo.java
@@ -27,29 +27,4 @@ public class EjercicioColaborativo {
     }
     
 }
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-package ejerciciocolaborativo;
-
-/**
- *
- * @author marti
- */
-public class EjercicioColaborativo {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("Proba bea");
-        System.out.println("Proba Xoel");
-        System.out.println("Proba Bea");
-        System.out.println("Proba para sincronizar");
-        System.out.println("Prueba CECA yeyeye");
-    }
-    
-}

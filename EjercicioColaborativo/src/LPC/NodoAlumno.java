f98017dcff143eca674e239b7701501025554c35
CECA <74151902+ceca-the-furtive@users.noreply.github.com>
2021-03-19 18:43:26 +0100

    Creo las clases lista, pila y cola. Creo el NodoAlumno.

---
+++ EjercicioColaborativo/src/LPC/NodoAlumno.java
@@ -0,0 +1,10 @@
+package LPC;
+
+import Clases.Alumno;
+
+public class NodoAlumno extends Alumno{
+
+    public NodoAlumno(String codigo, String nombreModulo1, float notaModulo1, String nombreModulo2, float notaModulo2, String nombre, String dni, String numeroTelefono) {
+        super(codigo, nombreModulo1, notaModulo1, nombreModulo2, notaModulo2, nombre, dni, numeroTelefono);
+    }
+}

772048d09fca089edc9d881355e22db797120094
DiegoLago <20dlagopaz01@gmail.com>
2021-03-22 18:54:34 +0100

    Diego

--- EjercicioColaborativo/src/LPC/ListaOrdenada.java
+++ EjercicioColaborativo/src/LPC/ListaOrdenada.java
@@ -26,7 +26,39 @@ public class ListaOrdenada {
         return p;
 
     }
+    public static NodoAlumno borrarNodo(NodoAlumno p, String nombreAlumnoBorrar) {
 
+        NodoAlumno q;
+        NodoAlumno actual = p;
+        NodoAlumno anterior = p;
 
+        if (p == null) {
+            System.out.println("La lista se encuentra vacia");
+        } else {
+            while (actual != null && !nombreAlumnoBorrar.equals(actual.getNombreAlumno())) {
+                anterior = actual;
+                actual = actual.getSiguiente();
+            }
+            if (actual == null) {
+                System.out.println("El elemento no se encuentra en la lista");
+            } else {
+                if (pedirValidacion("Desea eliminar al alumno " + actual.getNombreAlumno())) {
 
+                    if (anterior != actual) {
+                        anterior.setSiguiente(actual.getSiguiente());
+                    } else {
+                        p = p.getSiguiente();
+                    }
+                    System.out.println("Alumno eliminado ");
+                } else {
+                    System.out.println("Alumno no eliminado");
+                }
+
+            }
+
+        }
+        return p;
+
+    }
+    
 }
